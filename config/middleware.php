6917c6304a05e6b47eb66f5e6cb5a86934ca44bb
odan <d.opitz@outlook.com>
2020-04-04 11:34:54 +0200

    Init

--- config/middleware.php
+++ config/middleware.php
@@ -8,7 +8,7 @@ return function (App $app) {
     // Parse json, form data and xml
     $app->addBodyParsingMiddleware();
 
-    // Add global middleware to app
+    // Add the Slim built-in routing middleware
     $app->addRoutingMiddleware();
 
     // Catch exceptions and errors
